fd238570a70060fb9a73d38dc919bd328fea2e86
Dhanraj <dhanrajm@enlume.com>
2021-04-22 23:38:03 +0530

    Delete all

--- RMA-API/src/main/java/com/swift/rma/repository/RmaCountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.swift.rma.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.swift.rma.entity.RmaCountry;
-
-public interface RmaCountryRepository extends JpaRepository<RmaCountry, Long>{
-
-}
