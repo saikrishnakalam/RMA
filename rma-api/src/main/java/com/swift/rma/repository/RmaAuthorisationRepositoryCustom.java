89633147512bb093c03dd2ef28163eb83c467785
Dhanraj <dhanrajm@enlume.com>
2021-04-22 23:39:25 +0530

    First commit

---
+++ rma-api/src/main/java/com/swift/rma/repository/RmaAuthorisationRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.swift.rma.repository;
+
+import java.util.List;
+
+import javax.persistence.Tuple;
+
+import com.swift.rma.entity.RmaAuthorisation;
+import com.swift.rma.entity.RmaBic;
+
+public interface RmaAuthorisationRepositoryCustom {
+	public List<Tuple> getRelationsByFilter(String counterPartyText, List<String> issuerBics, List<String> correspondingBics, List<String> incomingAuthDirection, List<String> outgoingAuthDirection);
+}
---
+++ rma-api/src/main/java/com/swift/rma/service/RmaAuthorisationService.java
@@ -0,0 +1,22 @@
+package com.swift.rma.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.swift.rma.entity.RmaAuthorisation;
+import com.swift.rma.entity.RmaBic;
+import com.swift.rma.entity.RmaCountry;
+import com.swift.rma.model.RmaSearchResult;
+
+public interface RmaAuthorisationService {
+	
+	List<RmaSearchResult> getRelationsByFilter(String counterPartyText, List<String> issuerBics, List<String> correspondentBics, List<String> incomingAuthDirection, List<String> outgoingAuthDirection);
+	
+	List<RmaBic> getCounterPartyBics();
+	
+	List<RmaBic> getCounterPartyBicsByName(String counterPartyText);
+	
+	List<RmaCountry> getCountries();
+
+}
